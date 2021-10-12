e98bbbf516d0d0d06d4199576feaf9026347754c
Felipe Honorio Moraes <felipehonorio55@gmail.com>
2021-10-11 22:40:54 -0300

    readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # esp8266 alarm
 
+<<<<<<< Updated upstream
 <img src="images/pinos.png">
+=======
+<img src="images/pinos.jpg"> <br>
+<img src="images/pinos2.png">
+>>>>>>> Stashed changes
--- esp8266Alarm/esp8266Alarm.ino
+++ esp8266Alarm/esp8266Alarm.ino
@@ -1,12 +1,25 @@
 
-void setup() {
-  // put your setup code here, to run once:
 
+void setup() 
+{
+    // Instrução para colocar o gpio que iremos utilizar como entrada,
+    // podemos fazer a leitura nesse pino
+    pinMode(D1, INPUT); // D1 é uma constante que indica o pino que ligamos nosso botão
 
+    // Instrução para colocar o gpio que iremos utilizar como saída,
+    // podemos alterar seu valor livremente para HIGH ou LOW
+    pinMode(LED_BUILTIN, OUTPUT); // LED_BUILTIN é uma constante que indica o LED do ESP8266
+} 
 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
+    // faz a leitura do pino D1 (no nosso caso, o botão está ligado nesse pino)
+    byte valor = digitalRead(D1); 
+    // checa se o botão está pressionado
+    if(valor == HIGH) {         
+       digitalWrite(LED_BUILTIN, LOW);  // Acende o LED   pino  16
+    } 
+   else {
+       digitalWrite(LED_BUILTIN, HIGH);  // Apaga o LED
+    }
 }
